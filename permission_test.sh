5f89a48b9635c66e4a0161411a2544dfd4e020cc
Tarun M <2922986+tango4567@users.noreply.github.com>
2024-08-23 14:22:59 +0530

    Test Changes

---
+++ permission_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the directory to list
+DIRECTORY="$0"
+echo $DIRECTORY
+
+# # Check if the directory is provided
+# if [ -z "$DIRECTORY" ]; then
+#   echo "Usage: $0 <directory>"
+#   exit 1
+# fi
+
+# # Check if the directory exists
+# if [ ! -d "$DIRECTORY" ]; then
+#   echo "Error: '$DIRECTORY' is not a directory."
+#   exit 1
+# fi
+
+# # List files with detailed information, including hidden files and human-readable sizes
+# ls -lha "$DIRECTORY"
